c6952329f17445dc388cb28673370ae2b1808618
Hanhai <85443834+HanhaiNotHai@users.noreply.github.com>
2024-11-25 23:11:16 +0800

    watermark twice (#9)

--- decode.py
+++ decode.py
@@ -42,13 +42,15 @@ def main():
     inn_data = Test_Dataset(args.watermarked_image, "png")
     inn_loader = DataLoader(inn_data, batch_size=args.batch_size, shuffle=False, drop_last=True)
 
-    acc_history = []
+    acc_history1 = []
+    acc_history2_1 = []
+    acc_history2_2 = []
 
     with torch.no_grad():
         if args.noise_type in ["JPEG", "HEAVY"]:
             # fed_path = os.path.join("experiments", args.noise_type, "FED.pt")
             fed_path = os.path.join(
-                'experiments/1125_09:05:43/JPEGfed_220_49.11232dB_100.00000%.pt'
+                'experiments/1125_11:22:33/JPEGfed_420_36.91206dB_99.99349%.pt'
             )
             fed = FED().to(device)
             load(fed_path, fed)
@@ -60,36 +62,53 @@ def main():
                 load(inl_path, inl)
                 inl.eval()
 
-            for idx, watermarked_images in enumerate(inn_loader):
-                watermarked_images: Tensor = watermarked_images.to(device)
-                embedded_messgaes: Tensor = torch.load(
-                    os.path.join(args.messages_path, "message_{}.pt".format(idx + 1)),
+            for idx, (watermarked_images1, watermarked_images2) in enumerate(inn_loader):
+                watermarked_images1: Tensor = watermarked_images1.to(device)
+                watermarked_images2: Tensor = watermarked_images2.to(device)
+                embedded_messgaes1: Tensor = torch.load(
+                    os.path.join(args.messages_path, "message_{}_1.pt".format(idx + 1)),
                     map_location='cpu',
                     weights_only=True,
                 ).to(device)
-                key: Tensor = torch.load(
-                    os.path.join(args.messages_path, "key_{}.pt".format(idx + 1)),
+                key1: Tensor = torch.load(
+                    os.path.join(args.messages_path, "key_{}_1.pt".format(idx + 1)),
+                    map_location='cpu',
+                    weights_only=True,
+                ).to(device)
+                embedded_messgaes2: Tensor = torch.load(
+                    os.path.join(args.messages_path, "message_{}_2.pt".format(idx + 1)),
+                    map_location='cpu',
+                    weights_only=True,
+                ).to(device)
+                key2: Tensor = torch.load(
+                    os.path.join(args.messages_path, "key_{}_2.pt".format(idx + 1)),
                     map_location='cpu',
                     weights_only=True,
                 ).to(device)
 
-                all_zero = torch.zeros(embedded_messgaes.shape).to(device)
+                all_zero = torch.zeros(embedded_messgaes1.shape).to(device)
 
                 if args.noise_type == "HEAVY":
-                    watermarked_images = inl(watermarked_images.clone(), rev=True)
+                    watermarked_images1 = inl(watermarked_images1.clone(), rev=True)
 
-                reversed_img, extracted_messages, _ = fed(
-                    [watermarked_images, all_zero, key], rev=True
-                )
+                _, extracted_messages1, _ = fed([watermarked_images1, all_zero, key1], rev=True)
+                _, extracted_messages2_1, _ = fed([watermarked_images2, all_zero, key1], rev=True)
+                _, extracted_messages2_2, _ = fed([watermarked_images2, all_zero, key2], rev=True)
 
-                acc_rate = decoded_message_acc_rate(embedded_messgaes, extracted_messages)
+                acc_rate1 = decoded_message_acc_rate(embedded_messgaes1, extracted_messages1)
+                acc_rate2_1 = decoded_message_acc_rate(embedded_messgaes1, extracted_messages2_1)
+                acc_rate2_2 = decoded_message_acc_rate(embedded_messgaes2, extracted_messages2_2)
 
-                acc_history.append(acc_rate)
+                acc_history1.append(acc_rate1)
+                acc_history2_1.append(acc_rate2_1)
+                acc_history2_2.append(acc_rate2_2)
 
         else:
             raise ValueError("\"{}\" is not a valid noise type ".format(args.noise_type))
 
-    print(f'acc: {np.mean(acc_history):.5f}%')
+    print(
+        f'acc1: {np.mean(acc_history1):.5f}% | acc2_1: {np.mean(acc_history2_1):.5f}% | acc2_2: {np.mean(acc_history2_2):.5f}%'
+    )
 
 
 if __name__ == '__main__':
